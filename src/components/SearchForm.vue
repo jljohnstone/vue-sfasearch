2a2388ca313b4af2434ed217d75922575e09dfd4
Jason Johnstone <jljohnstone@sfasu.edu>
2016-08-11 13:45:38 -0500

    basic search form with input and button; dispatches search terms to parent for processing

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit="requestSearch">
+    <input type="text" v-model="terms">
+    <button type="submit">Search</button>
+  </form>
+  <p><strong>Terms: {{ terms }}</strong></p>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      terms: 'scott'
+    }
+  },
+  methods: {
+    requestSearch (e) {
+      e.preventDefault()
+      this.$dispatch('new-search', this.terms)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
