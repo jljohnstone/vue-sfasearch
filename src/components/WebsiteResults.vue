ad22671402bf568d09b51126616cc1c135cf2552
Jason Johnstone <jljohnstone@sfasu.edu>
2017-03-09 10:20:25 -0600

    only display 'include documents' checkbox when website search results are present

--- src/components/WebsiteResults.vue
+++ src/components/WebsiteResults.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="website-results">
     <h2>Website Results</h2>
-    <label><input type="checkbox" v-model="include_docs" v-on:click="toggleDocs">Include documents</label>
-    <ol class="website-results-list" v-if="count">
-      <li v-for="entry in website.result" class="website-result">
-        <div class="result-title">
-          <i class="fa fa-file-pdf-o" aria-hidden="true" title="PDF file" v-if="entry.filetype === 'pdf'">&nbsp;</i>
-          <a href="{{ entry.url_link }}">
-            {{ entry.content_title || "untitled document"}}
-          </a>
-          <sup class="filetype" v-if="entry.filetype === 'pdf'">[PDF]</sup>
-        </div>
-        <div class="result-body">
-          {{{ entry.content_description }}}
-        </div>
-        <div class="result-url">
-          {{ entry.url_link }}
-        </div>
-      </li>
-    </ol>
+    <div v-if="count">
+    <label><input type="checkbox" v-model="include_docs" v-on:click="toggleDocs"> Include documents</label>
+      <ol class="website-results-list">
+        <li v-for="entry in website.result" class="website-result">
+          <div class="result-title">
+            <i class="fa fa-file-pdf-o" aria-hidden="true" title="PDF file" v-if="entry.filetype === 'pdf'">&nbsp;</i>
+            <a href="{{ entry.url_link }}">
+              {{ entry.content_title || "untitled document"}}
+            </a>
+            <sup class="filetype" v-if="entry.filetype === 'pdf'">[PDF]</sup>
+          </div>
+          <div class="result-body">
+            {{{ entry.content_description }}}
+          </div>
+          <div class="result-url">
+            {{ entry.url_link }}
+          </div>
+        </li>
+      </ol>
+    </div>
     <p v-else>
       <strong>There were no relevant webpages found.</strong>
     </p>
